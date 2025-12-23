6869f4ad8efb87a000c4ac092ac36dc3f6a9aa99
Mark-Kadphol Lenglerdphol <kadphol.l@7solutions.co.th>
2025-12-23 14:35:17 +0700

    game state

---
+++ src/init.js
@@ -0,0 +1,20 @@
+import gameState from './game-state'
+const TICK_RATE = 3000
+
+async function init() {
+  console.log('starting game')
+
+  let nextTimeToTick = Date.now()
+  function nextAnimationFrame() {
+    const now = Date.now()
+    if (nextTimeToTick <= now) {
+      gameState.tick()
+      nextTimeToTick = now + TICK_RATE
+    }
+    requestAnimationFrame(nextAnimationFrame)
+  }
+
+  nextAnimationFrame()
+}
+
+init()
