5138f68fd9eeac2df839a89ca4037f409a8d9d67
Mark-Kadphol Lenglerdphol <kadphol.l@7solutions.co.th>
2025-12-23 14:56:25 +0700

    Adding UI

--- src/init.js
+++ src/init.js
@@ -1,8 +1,10 @@
 import gameState from './game-state'
-const TICK_RATE = 3000
+import { TICK_RATE } from './constants'
+import initButtons from './button'
 
 async function init() {
   console.log('starting game')
+  initButtons(gameState.handleUserAction)
 
   let nextTimeToTick = Date.now()
   function nextAnimationFrame() {
